a80fed2971acda838455d6dc9f7ce59438188f3f
nayabrasoolsb <nrasool030@gmail.com>
2022-12-30 17:52:34 +0530

    some basic files in backend  are updated

---
+++ back-end/index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+const connection = require("./connection/connection.js");
+const loginRoutes = require("./routes/login.js");
+const blogsRoutes = require("./routes/blogs.js");
+const userRoutes = require("./routes/user.js");
+const cors = require("cors");
+var jwt = require("jsonwebtoken");
+const secret = "RESTAPI";
+
+
+app.use(cors())
+app.use("/api/v1/blogs", (req, res, next) => {
+  if (req.headers.authorization) {
+    const token = req.headers.authorization;
+    // console.log("Verify token");
+    if (token) {
+      // verify a token symmetric
+      jwt.verify(token, secret, (err, decoded)=> {
+        if (err) {
+          return res.status(403).json({
+            status: "failed",
+            message: "Invalid token",
+          });
+        }
+        req.user = decoded.data;
+        // console.log(req.user)
+        next();
+      });
+    } else {
+      return res.status(403).json({
+        status: "failed",
+        message: "Invalid token",
+      });
+    }
+  } else {
+    return res
+      .status(403)
+      .json({ status: "Failed", message: "Not authenticated user" });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.status(200).send("ok");
+});
+
+app.use("/api/v1/blogs", blogsRoutes);
+app.use("/api/v1/user", loginRoutes);
+
+app.use("/api/v1/users", userRoutes)
+
+app.get("*", (req, res) => {
+  res.status(404).send("page not found");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("server is up in ", process.env.PORT);
+});
